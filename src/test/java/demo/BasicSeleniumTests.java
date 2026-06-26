87db079eacb4d674cd8bb0308caefabf51cadce7
Latifa M. Issa <lissa@thiqah.sa>
2026-06-26 13:38:31 +0300

    add first ex

--- src/test/java/demo/BasicSeleniumTests.java
+++ src/test/java/demo/BasicSeleniumTests.java
@@ -1,6 +1,18 @@
 package demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BasicSeleniumTests {
+  @Test
+    public void basicSeleniumTests() {
+      WebDriver driver ;
+      //Test implementation
+      driver= new ChromeDriver();
+      driver.navigate().to("https://www.google.com");
+      driver.quit();
 
+    }
 
+    
 }
